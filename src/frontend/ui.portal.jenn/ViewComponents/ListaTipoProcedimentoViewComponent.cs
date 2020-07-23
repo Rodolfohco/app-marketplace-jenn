8300468fa36a51469eb76d13d3d3cbddad1d0d4a
Rodolfo Hernani <rodolfohco@hotmail.com>
2020-07-22 22:20:25 -0300

    Lista TIpo Procedimento

---
+++ src/frontend/ui.portal.jenn/ViewComponents/ListaTipoProcedimentoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ui.portal.jenn.ViewComponents
+{
+    public class ListaTipoProcedimentoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(
+            string sequencia)
+        {
+            var valores = await Task.FromResult(
+                sequencia.Split(new char[] { '|' }));
+            return View(valores);
+        }
+    }
+    
+
+}
